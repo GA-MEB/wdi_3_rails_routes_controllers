efc165c09d23fc3e3c08e19340d3a6ebafda0d11
Tom Dyer <tdyer@ourstage.com>
2014-05-05 17:56:18 -0400

    Added Song model and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505200901) do
+ActiveRecord::Schema.define(version: 20140505214900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20140505200901) do
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "songs", force: true do |t|
+    t.string   "title"
+    t.string   "genre"
     t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
