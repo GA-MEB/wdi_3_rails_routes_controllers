688879165966a5fc86d05cb7fec435a0a40ef647
Tom Dyer <tdyer@ourstage.com>
2014-05-05 16:21:40 -0400

    Added the Product model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: "Brown Shoe", description: "It's brown, it's a shoe", price: 13.99)
+Product.create(name: "Rose Colored Glasses", description: "To make you happy, happy", price: 23.49)
+Product.create(name: 'Motorized Fork', description: 'To stuff your gob more effienctly', price: 12.07)
+Product.create(name: 'Brown Paper Bag', description: "Hide your booze, openly, in public for everyone to see", price: 1.44)
+Product.create(name: 'Bouncy Castle', description: 'Perfect software development environment',  price: 1_200.00)
+Product.create(name: 'Headphones', description: 'Phone for you head', price: 23.66)
+
